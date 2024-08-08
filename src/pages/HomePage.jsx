3b26e3ebda8cffcfda3f4e37ac6de01cb3babe7c
FasadSalatov <salatiksama@gmail.com>
2024-08-09 00:14:10 +0300

    yes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     const [progress, setProgress] = useState(0);
     const [level, setLevel] = useState(1);
     const [maxLevel, setMaxLevel] = useState(3);
-    const id = 2;
+    const id = tg?.initDataUnsafe?.user?.id;
 
     const [boostData, setBoostData] = useState({
         fullEnergy: { available: 0, remainingMinutes: 0, count: 0 },
@@ -23,7 +23,7 @@ const HomePage = () => {
     });
 
     const [userData, setUserData] = useState({
-        user_id: 2,
+        user_id: tg?.initDataUnsafe?.user?.id,
         balance: 0,
         coins_per_day: 0,
         wallet: ''
